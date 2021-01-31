b3228fa9cc739aa38fa3e5104d11250a5409cbd3
NGO SANG <46277436+ngosangns@users.noreply.github.com>
2021-01-31 16:27:06 +0700

    Config docker

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: my-unsplash
--- setenv_cmd.cmd
+++
@@ -1,4 +0,0 @@
-setx PORT "80"
-setx DBCONNECTSTRING "mongodb+srv://ngosangns:jikmli@cluster0.oxs6m.mongodb.net/ngosangns?retryWrites=true&w=majority"
-setx DBNAME "admin"
-setx DBCOLLECTION "my-unsplash"
